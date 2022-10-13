35c6e62bde10bfd397aa46e82697bd0bfb97bfa9
misonou <misonou.mirai@gmail.com>
2022-10-13 12:45:58 +0800

    0.3.1

--- dist/zeta.js
+++ dist/zeta.js
@@ -1228,12 +1228,17 @@ function registerCleanup(element, callback) {
   }
 }
 
-function createAutoCleanupMap(callback) {
+function createAutoCleanupMap(container, callback) {
+  if (!container || isFunction(container)) {
+    callback = container;
+    container = root;
+  }
+
   var map = new Map();
   callback = isFunction(callback) || noop;
-  observe(root, function () {
+  observe(container, function () {
     each(map, function (i) {
-      if (!root.contains(i)) {
+      if (!container.contains(i)) {
         callback(i, mapRemove(map, i));
       }
     });
@@ -1260,15 +1265,11 @@ function afterDetached(element, from, callback) {
   return promise;
 }
 
-function watchElements(element, selector, callback, fireInit) {
+function trackElements(element, selector) {
   var collection = new Set();
   var add = collection.add.bind(collection);
   var remove = collection.delete.bind(collection);
-  var options = extend({}, optionsForChildList, {
-    attributes: selector.indexOf('[') >= 0
-  });
-
-  var handler = function handler() {
+  return function (callback) {
     var matched = selectIncludeSelf(selector, element);
     var removedNodes = grep(collection, function (v) {
       return matched.indexOf(v) < 0;
@@ -1280,42 +1281,57 @@ function watchElements(element, selector, callback, fireInit) {
     if (addedNodes[0] || removedNodes[0]) {
       addedNodes.forEach(add);
       removedNodes.forEach(remove);
-      callback(addedNodes, removedNodes);
+      (callback || noop)(addedNodes, removedNodes);
     }
+
+    return [addedNodes, removedNodes];
   };
+}
 
-  if (fireInit) {
-    handler();
-  }
-
-  return observe(element, options, handler);
+function watchElements(element, selector, callback, fireInit) {
+  var collect = trackElements(element, selector);
+  var options = extend({}, optionsForChildList, {
+    attributes: selector.indexOf('[') >= 0
+  });
+  var fn = observe(element, options, function () {
+    collect(callback);
+  });
+  collect(fireInit && callback);
+  return fn;
 }
 
 function watchAttributes(element, attributes, callback, fireInit) {
-  var options = {
-    subtree: true,
+  var collect = trackElements(element, '[' + makeArray(attributes).join('],[') + ']');
+  var options = extend({}, optionsForChildList, {
     attributes: true,
     attributeFilter: makeArray(attributes)
-  };
+  });
   var fn = observe(element, options, function (records) {
     var set = new Set();
+    var arr = [[]].concat(collect());
+    var all = arr[1].concat(arr[2]);
     each(records, function (i, v) {
-      set.add(v.target);
-    });
-    callback(makeArray(set));
-  });
+      var target = v.target;
 
-  if (fireInit) {
-    domReady.then(function () {
-      var matched = selectIncludeSelf('[' + options.attributeFilter.join('],[') + ']', element);
-
-      if (matched[0]) {
-        callback(matched);
+      if (v.attributeName && all.indexOf(target) < 0 && set.add(target)) {
+        all.push(target);
       }
     });
-  }
-
+    arr[0] = all;
+    arr[3] = makeArray(set);
+    callback.apply(this, arr);
+  });
+  collect(fireInit && function (added) {
+    callback(added, added, [], []);
+  });
   return fn;
+}
+
+function watchOwnAttributes(element, attributes, callback) {
+  return observe(element, {
+    attributes: true,
+    attributeFilter: makeArray(attributes)
+  }, callback);
 }
 
 function initDetachWatcher(element) {
@@ -2646,7 +2662,7 @@ function setFocus(element, source, path, suppressFocusChange) {
       return focusFriends.get(v);
     })[0];
 
-    if (friend && !focused(friend)) {
+    if (friend && added.indexOf(friend) < 0 && !focused(friend)) {
       result = setFocus(friend, null, null, true);
     }
 
@@ -3475,7 +3491,8 @@ function dom_focus(element) {
   createAutoCleanupMap: createAutoCleanupMap,
   afterDetached: afterDetached,
   watchElements: watchElements,
-  watchAttributes: watchAttributes
+  watchAttributes: watchAttributes,
+  watchOwnAttributes: watchOwnAttributes
 });
 
 // CONCATENATED MODULE: ./src/events.js
@@ -4571,32 +4588,8 @@ function makeSelection(b, e) {
   var range = env_document.createRange();
   range.setStart(b.startContainer, b.startOffset);
   range.setEnd(e.endContainer, e.endOffset);
-
-  try {
-    selection.removeAllRanges();
-  } catch (e) {
-    // IE fails to clear ranges by removeAllRanges() in occasions mentioned in
-    // http://stackoverflow.com/questions/22914075
-    // @ts-ignore: non-standard member
-    var r = env_document.body.createTextRange();
-    r.collapse();
-    r.select();
-    selection.removeAllRanges();
-  }
-
-  try {
-    selection.addRange(range);
-  } catch (e) {
-    // IE may throws unspecified error even though the selection is successfully moved to the given range
-    // if the range is not successfully selected retry after selecting other range
-    if (!selection.rangeCount) {
-      var r1 = env_document.createRange();
-      r1.selectNode(env_document.body);
-      selection.addRange(r1);
-      selection.removeAllRanges();
-      selection.addRange(range);
-    }
-  }
+  selection.removeAllRanges();
+  selection.addRange(range);
 }
 
 function getRect(elm, includeMargin) {
