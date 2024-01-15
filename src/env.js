f5bf9bbc6df7ddea399fe06bb72a7137dc4339b1
misonou <misonou.mirai@gmail.com>
2024-01-15 12:23:31 +0800

    fix(env): detection for ipad

--- src/env.js
+++ src/env.js
@@ -9,8 +9,8 @@ export const getSelection = window.getSelection;
 export const getComputedStyle = window.getComputedStyle;
 export const domReady = new Promise($);
 
-export const IS_IOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+export const IS_MAC = navigator.userAgent.indexOf('Macintosh') >= 0;
+export const IS_IOS = (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) || (IS_MAC && navigator.maxTouchPoints > 2);
 export const IS_IE10 = !!window.ActiveXObject;
 export const IS_IE = IS_IE10 || root.style.msTouchAction !== undefined || root.style.msUserSelect !== undefined;
-export const IS_MAC = navigator.userAgent.indexOf('Macintosh') >= 0;
 export const IS_TOUCH = 'ontouchstart' in window;
