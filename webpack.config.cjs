8938334258e99e8d98fc4c1df91d1f4a86fc2e1e
misonou <misonou.mirai@gmail.com>
2024-05-10 11:56:42 +0800

    chore: migrate to build utils

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,69 +1,18 @@
-const fs = require('fs');
-const path = require('path');
-const webpack = require('webpack');
-const TerserPlugin = require('terser-webpack-plugin');
-
-const packageJSON = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+const { createBaseWebpackConfig, createUMDExternal, createUMDLibraryDefinition, getPaths } = require('@misonou/build-utils');
+const paths = getPaths();
 
 module.exports = {
+    ...createBaseWebpackConfig(),
     entry: {
         'zeta': './src/index.js',
         'zeta.min': './src/index.js',
     },
-    devtool: 'source-map',
     output: {
-        path: path.join(process.cwd(), 'dist'),
+        path: paths.dist,
         filename: '[name].js',
-        library: {
-            name: {
-                commonjs: 'zeta-dom',
-                amd: 'zeta-dom',
-                root: 'zeta'
-            },
-            type: 'umd',
-            umdNamedDefine: true
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
-    },
-    plugins: [
-        new webpack.BannerPlugin({
-            banner: `${packageJSON.name} v${packageJSON.version} | (c) ${packageJSON.author} | ${packageJSON.homepage}`
-        }),
-    ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                test: /\.min\.js$/i,
-                extractComments: false,
-                terserOptions: {
-                    format: {
-                        comments: 'some'
-                    }
-                }
-            })
-        ]
+        library: createUMDLibraryDefinition('zeta-dom', 'zeta')
     },
     externals: {
-        'promise-polyfill': 'promise-polyfill',
-        'jquery': {
-            commonjs: 'jquery',
-            commonjs2: 'jquery',
-            amd: 'jquery',
-            root: 'jQuery'
-        }
+        'jquery': createUMDExternal('jquery', 'jQuery')
     }
 };
