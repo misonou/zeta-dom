a64090abae3352e68a9c35464df60630146801c3
misonou <misonou.mirai@gmail.com>
2026-04-23 12:18:07 +0800

    chore: add error code descriptions

--- src/errorCode.d.ts
+++ src/errorCode.d.ts
@@ -1,3 +1,12 @@
+/**
+ * Indicates cancellation requested using `cancelLock()` is rejected.
+ */
 export declare const cancellationRejected: 'zeta/cancellation-rejected';
+/**
+ * Indicates an operation is cancelled.
+ */
 export declare const cancelled: 'zeta/cancelled';
+/**
+ * Indicates an invalid usage from library functions.
+ */
 export declare const invalidOperation: 'zeta/invalid-operation';
